135b3890970941cdd15a2e0e3f2ad5baa02923bb
Eric Dauenhauer <eric@ericyd.com>
2020-08-19 12:26:43 -0500

    Hurricane, geode, something else?

---
+++ openrndr/src/main/kotlin/sketch/flow/F3_Hurricane.kt
@@ -0,0 +1,105 @@
+/**
+ * A gentle gravity field, with some noise.
+ *
+ * The idea here is that each flow field is a particle that is seeking a target.
+ * The particle has a certain amount of speed with which it pursues the target,
+ * but it is also affected by the gravity field around it.
+ *
+ * The color of the particle is defined by a custom palette
+ */
+package sketch.flow
+
+import color.Palette
+import force.AntigravityBody
+import force.GravityBody
+import force.GravitySystem
+import force.PhysicalBody
+import org.openrndr.application
+import org.openrndr.color.ColorRGBa
+import org.openrndr.color.hsl
+import org.openrndr.draw.LineCap
+import org.openrndr.extensions.Screenshots
+import org.openrndr.extra.noise.random
+import org.openrndr.math.Vector2
+import org.openrndr.math.map
+import org.openrndr.shape.ShapeContour
+import org.openrndr.shape.contour
+import kotlin.random.Random
+
+fun main() = application {
+  configure {
+    width = 1000
+    height = 1000
+  }
+
+  program {
+    // In future, consider implementing own screenshot mechanism that could run after each draw loop
+    // screenshot: https://github.com/openrndr/openrndr/blob/9f17eb3c24813454cbad1a99d697cd279fa80d96/openrndr-extensions/src/main/kotlin/org/openrndr/extensions/Screenshots.kt
+    // timestamp: https://github.com/openrndr/openrndr/blob/9f17eb3c24813454cbad1a99d697cd279fa80d96/openrndr-core/src/main/kotlin/org/openrndr/utils/NamedTimestamp.kt
+    extend(Screenshots()) {
+      quitAfterScreenshot = false
+      scale = 2.0
+    }
+    backgroundColor = ColorRGBa.WHITE
+    val lineLength = 1500
+    val seed = random(1.0, 1000000000000.0).toLong() // know your seed 😛
+    println("seed: $seed")
+    val rand = Random(seed)
+    val padding = 300
+
+    val colors = listOf(
+      hsl(261.0, 0.45, 0.43), // purple
+      hsl(238.0, 0.67, 0.36), // dark blue
+      hsl(194.0, 0.70, 0.85), // light blue
+      hsl(10.0, 0.40, 0.15), // dark brown
+      hsl(255.0, 0.46, 0.86), // light purple
+      hsl(173.0, 0.66, 0.975), // smokey white
+      hsl(29.0, 0.93, 0.83) // orange/salmon
+    )
+
+    // val palette = Palette(colors.map { it.toRGBa() }, 0.0, hypot(width.toDouble(), height.toDouble()), rand)
+    val palette = Palette(colors.map { it.toRGBa() }, 0.0, width + padding * 2.0, rand)
+
+    // these attract things
+    val yOffset = 100.0
+    val gravityBodies: List<GravityBody> = listOf(
+      GravityBody(width * 0.1, height * 1.1, 100000.0),
+      GravityBody(width * -0.95, height * 0.15, 70000.0),
+      GravityBody(width * 1.2, height * 0.20, 150000.0)
+    )
+
+    // these repel things
+    val primaryAntigravityBodies = listOf(
+      AntigravityBody(width * -0.05, height + yOffset * 0.5, 100000.0),
+      AntigravityBody(width * 0.85, -yOffset * 0.5, 150000.0)
+    )
+
+    // this calculates net forces on a point mass
+    val system = GravitySystem(1.0, gravityBodies + primaryAntigravityBodies)
+
+    val stepSize = 20
+    val target = Vector2(width * 1.1, height * 1.1)
+    val contours: List<ShapeContour> = (-padding until width + padding step stepSize).flatMap { x ->
+      (-padding until height + padding step stepSize).map { y ->
+        val body = PhysicalBody(Vector2(random(x * 0.95, x * 1.05, rand), random(y * 0.95, y * 1.05, rand)), random(50.0, 150.0, rand), random(15.0, 25.0, rand), target)
+        contour {
+          moveTo(body.coords)
+          List(lineLength) {
+            lineTo(body.move(system))
+          }
+        }
+      }
+    }
+
+    extend {
+      drawer.strokeWeight = 0.55
+      drawer.lineCap = LineCap.ROUND
+      contours.forEach {
+        val start = it.segments.first().start
+        drawer.stroke = palette.colorAt(start.x).opacify(0.885)
+        drawer.contour(it)
+      }
+      drawer.circle(gravityBodies[0].origin, 10.0)
+    }
+  }
+}
--- openrndr/src/main/kotlin/sketch/test/T2_NoiseMap.kt
+++ openrndr/src/main/kotlin/sketch/test/T2_NoiseMap.kt
@@ -15,7 +15,7 @@ import color.Palette
 import org.openrndr.application
 import org.openrndr.color.ColorRGBa
 import org.openrndr.color.hsl
-import org.openrndr.math.Vector2
+import org.openrndr.extra.noise.random
 import kotlin.random.Random
 
 fun main() = application {
@@ -28,14 +28,17 @@ fun main() = application {
     backgroundColor = ColorRGBa.BLACK
 
     val colors = listOf(
-      hsl(250.0, 0.5, 0.5),
-      hsl(275.0, 0.5, 0.5),
-      hsl(2.0, 0.25, 0.4),
-      hsl(300.0, 0.65, 0.6),
-      hsl(25.0, 0.5, 0.6)
+      hsl(261.0, 0.45, 0.43), // purple
+      hsl(238.0, 0.67, 0.36), // dark blue
+      hsl(194.0, 0.70, 0.85), // light blue
+      hsl(10.0, 0.40, 0.15), // dark brown
+      hsl(255.0, 0.46, 0.86), // light purple
+      hsl(173.0, 0.66, 0.975), // smokey white
+      hsl(29.0, 0.93, 0.83) // orange/salmon
     )
 
-    val seed = 100
+    val seed = random(1.0, 10000.0).toInt()
+    println("seed: $seed")
     val rand = Random(seed)
 
     val palette = Palette(colors.map { it.toRGBa() }, 0.0, width.toDouble(), rand)
@@ -44,18 +47,18 @@ fun main() = application {
       /**
        * Either of the below methods works. You can either draw rectangles, or draw lines and figure out where they fit
        */
-      // palette.forEach { color, x, w ->
-      //   drawer.fill = color
-      //   drawer.rectangle(x, 0.0, w, height.toDouble())
-      // }
-
-      drawer.strokeWeight = 2.0
-      (0 until width step 4).forEach { x ->
-        var color = palette.colorAt(x.toDouble())
-        drawer.stroke = color
-        // drawer.stroke = ColorRGBa.WHITE
-        drawer.lineSegment(Vector2(x.toDouble(), 0.0), Vector2(x.toDouble(), height.toDouble()))
+      palette.forEach { color, x, w ->
+        drawer.fill = color
+        drawer.rectangle(x, 0.0, w, height.toDouble())
       }
+
+      // drawer.strokeWeight = 2.0
+      // (0 until width step 4).forEach { x ->
+      //   var color = palette.colorAt(x.toDouble())
+      //   drawer.stroke = color
+      //   // drawer.stroke = ColorRGBa.WHITE
+      //   drawer.lineSegment(Vector2(x.toDouble(), 0.0), Vector2(x.toDouble(), height.toDouble()))
+      // }
     }
   }
 }

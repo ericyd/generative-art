82bc7aa66a56bee0905a376e888b9cc9a8392335
Eric Dauenhauer <eric@ericyd.com>
2020-11-18 21:48:22 -0600

    Update to Kotlin 1.4.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -96,7 +96,7 @@ enum class Logging {
 /*  What type of logging should this project use? */
 val applicationLogging = Logging.FULL
 
-val kotlinVersion = "1.3.72"
+val kotlinVersion = "1.4.10"
 
 plugins {
   java

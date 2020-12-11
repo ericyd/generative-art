5b84c82ac459102e6ba8df9b94a3ea00db6c6d0e
Eric Dauenhauer <eric@ericyd.com>
2020-12-10 20:43:30 -0600

    Publish "glowing graph" to site

---
+++ openrndr/src/main/kotlin/sketch/test/T14_Blur_Screenshot_Scale.kt
@@ -0,0 +1,88 @@
+/**
+ * Create demo of how to save a "high res" version of an image with blur and compositor
+ */
+package sketch.test
+
+import org.openrndr.application
+import org.openrndr.color.ColorRGBa
+import org.openrndr.draw.BufferMultisample
+import org.openrndr.draw.TransformTarget
+import org.openrndr.draw.isolatedWithTarget
+import org.openrndr.draw.renderTarget
+import org.openrndr.extensions.Screenshots
+import org.openrndr.extra.compositor.blend
+import org.openrndr.extra.compositor.compose
+import org.openrndr.extra.compositor.draw
+import org.openrndr.extra.compositor.layer
+import org.openrndr.extra.compositor.post
+import org.openrndr.extra.fx.blend.Add
+import org.openrndr.extra.fx.blur.ApproximateGaussianBlur
+import org.openrndr.shape.Circle
+import util.timestamp
+import java.io.File
+
+
+fun main() = application {
+  configure {
+    width = 500
+    height = 500
+  }
+
+  program {
+    extend(Screenshots()) {
+      scale = 1.0 // <-- increasing causes screenshot to change from original image
+      multisample = BufferMultisample.SampleCount(8)
+    }
+
+    val blur = ApproximateGaussianBlur().apply {
+      window = 25
+      sigma = 15.0
+      spread = 5.0
+      gain = 3.0
+    }
+
+    val rndrTarget = renderTarget(750, 750, multisample = BufferMultisample.Disabled) {
+      colorBuffer()
+      depthBuffer()
+    }
+
+    val c = Circle(width / 2.0, height / 2.0, width / 4.0)
+
+    extend {
+      val composite = compose {
+        layer {
+          draw {
+            drawer.fill = ColorRGBa.PINK.opacify(0.5)
+            drawer.circle(c)
+          }
+        }
+        layer {
+          blend(Add())
+          draw {
+            drawer.fill = null
+            drawer.stroke = ColorRGBa.PINK
+            drawer.circle(c)
+          }
+          post(blur)
+        }
+      }
+
+      drawer.isolatedWithTarget(rndrTarget) {
+        // image(rt.colorBuffer(0))
+        composite.draw(drawer)
+      }
+
+      val printTarget = renderTarget(rndrTarget.width, rndrTarget.height, rndrTarget.contentScale, rndrTarget.multisample, rndrTarget.session) {
+        colorBuffer()
+      }
+
+      drawer.isolatedWithTarget(printTarget) {
+        drawer.ortho(rndrTarget)
+        image(rndrTarget.colorBuffer(0))
+      }
+      printTarget.colorBuffer(0).saveToFile(File("screenshot-${timestamp()}.png"), async = false)
+      drawer.scale(width.toDouble() / rndrTarget.width, TransformTarget.MODEL)
+      drawer.image(rndrTarget.colorBuffer(0))
+    }
+  }
+}
---
+++ openrndr/src/main/kotlin/sketch/test/T15_Blur_Screenshot_Scale2.kt
@@ -0,0 +1,70 @@
+/**
+ * Goal:
+ * Demo for issue with scaled screenshots with blur
+ */
+package sketch.test
+
+import org.openrndr.application
+import org.openrndr.color.ColorRGBa
+import org.openrndr.draw.BufferMultisample
+import org.openrndr.draw.ColorFormat
+import org.openrndr.draw.ColorType
+import org.openrndr.draw.TransformTarget
+import org.openrndr.draw.isolatedWithTarget
+import org.openrndr.draw.renderTarget
+import org.openrndr.extensions.Screenshots
+import org.openrndr.extra.compositor.blend
+import org.openrndr.extra.compositor.compose
+import org.openrndr.extra.compositor.draw
+import org.openrndr.extra.compositor.layer
+import org.openrndr.extra.compositor.post
+import org.openrndr.extra.fx.blend.Add
+import org.openrndr.extra.fx.blur.ApproximateGaussianBlur
+import org.openrndr.shape.Circle
+import util.timestamp
+import java.io.File
+
+fun main() = application {
+  configure {
+    width = 500
+    height = 500
+  }
+
+  program {
+    extend(Screenshots()) {
+      scale = 3.0 // <-- increasing causes screenshot to change from original image
+      multisample = BufferMultisample.SampleCount(8)
+    }
+
+    val blur = ApproximateGaussianBlur().apply {
+      window = 25
+      sigma = 15.0
+      spread = 5.0
+      gain = 3.0
+    }
+
+    val c = Circle(width / 2.0, height / 2.0, width / 4.0)
+
+    extend {
+      val composite = compose {
+        layer {
+          draw {
+            drawer.fill = ColorRGBa.PINK.opacify(0.5)
+            drawer.circle(c)
+          }
+        }
+        layer {
+          blend(Add())
+          draw {
+            drawer.fill = null
+            drawer.stroke = ColorRGBa.PINK
+            drawer.circle(c)
+          }
+          post(blur)
+        }
+      }
+
+      composite.draw(drawer)
+    }
+  }
+}

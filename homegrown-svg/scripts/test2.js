7c9aee0a08f50b75c03a698c4ae2429b24b8d448
Eric Dauenhauer <eric@ericyd.com>
2022-06-11 14:04:53 -0500

    formatting

--- homegrown-svg/scripts/test2.js
+++ homegrown-svg/scripts/test2.js
@@ -1,7 +1,7 @@
 import { svg, tag } from "../lib/tag.js";
 import { array } from "../lib/util.js";
 import { pathBuilder, randomPoint } from "../lib/path.js";
-import { rngFactory } from '../lib/random.js'
+import { rngFactory } from "../lib/random.js";
 
 export function draw() {
   const root = svg({
@@ -16,27 +16,40 @@ export function draw() {
 
   const colors = ["#fff", "#faf", "#fb4", "#4ff"];
 
-  const seed = Date.now().toString()
-  console.log({seed})
-  window.location.hash = seed
-  const rng = rngFactory(seed)
+  const seed = Date.now().toString();
+  console.log({ seed });
+  window.location.hash = seed;
+  const rng = rngFactory(seed);
 
   const children = array(10).map((i) =>
-    path.withAttrs({ d: pathBuilder((d) => {
-      d.move(randomPoint(20, 80, 20, 80, rng))
-      let i = 0
-      while (i < 50) {
-        if (i % 15 === 0) {
-          d.smoothBezier(randomPoint(20, 80, 20, 80, rng), randomPoint(20, 80, 20, 80, rng), 'absolute')
-          // d.smoothBezier(randomPoint(-5, 5, -5, 5, rng), randomPoint(-5, 5, -5, 5, rng), 'relative')
-        } else if (i % 3 === 0) {
-          d.smoothBezier(randomPoint(-5, 5, -5, 5, rng), randomPoint(-5, 5, -5, 5, rng), 'relative')
-        } else {
-          d.smoothBezier(randomPoint(-3, 3, -3, 3, rng), randomPoint(-3, 3, -3, 3, rng), 'relative')
+    path.withAttrs({
+      d: pathBuilder((d) => {
+        d.move(randomPoint(20, 80, 20, 80, rng));
+        let i = 0;
+        while (i++ < 50) {
+          if (i % 15 === 0) {
+            d.smoothBezier(
+              randomPoint(20, 80, 20, 80, rng),
+              randomPoint(20, 80, 20, 80, rng),
+              "absolute"
+            );
+          } else if (i % 3 === 0) {
+            d.smoothBezier(
+              randomPoint(-5, 5, -5, 5, rng),
+              randomPoint(-5, 5, -5, 5, rng),
+              "relative"
+            );
+          } else {
+            d.smoothBezier(
+              randomPoint(-3, 3, -3, 3, rng),
+              randomPoint(-3, 3, -3, 3, rng),
+              "relative"
+            );
+          }
         }
-        i++
-      }
-    }), stroke: colors[i % 4] })
+      }),
+      stroke: colors[i % 4],
+    })
   );
 
   return root.withChildren(children).draw();

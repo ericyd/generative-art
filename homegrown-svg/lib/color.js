2b56f1c78cc20944d4543695e0c7a56d815e7188
Eric Dauenhauer <eric@ericyd.com>
2022-07-08 12:56:31 -0500

    fix colors

--- homegrown-svg/lib/color.js
+++ homegrown-svg/lib/color.js
@@ -1,54 +1,33 @@
 import { clamp } from "./util.js";
 
-export class Hsl {
-  constructor(h, s, l) {
-    if (h < 0 || h > 360) {
-      console.warn(`h is outside of [0, 360]: ${h}. Clamped to ${h2}`);
-    }
-    if (s < 0 || s > 100) {
-      console.warn(`s is outside of [0, 100]: ${s}. Clamped to ${s2}`);
-    }
-    if (l < 0 || l > 100) {
-      console.warn(`l is outside of [0, 100]: ${l}. Clamped to ${l2}`);
-    }
-    this.h = clamp(0, 360, h);
-    this.s = clamp(0, 100, s);
-    this.l = clamp(0, 100, l);
-  }
-
-  toString() {
-    return `hsl(${this.h}, ${this.s}%, ${this.l}%)`;
-  }
-}
-
 /**
  * @param {number} h 
  * @param {number} s 
  * @param {number} l 
- * @returns {Hsl} color in hsl format
+ * @returns {Hsla} color in hsl format
  */
 export function hsl(h, s, l) {
-  return new Hsl(h, s, l)
+  return new Hsla(h, s, l, 1.0)
 }
 
 export class Hsla {
   constructor(h, s, l, a) {
-    if (h < 0 || h > 360) {
-      console.warn(`h is outside of [0, 360]: ${h}. Clamped to ${h2}`);
-    }
-    if (s < 0 || s > 100) {
-      console.warn(`s is outside of [0, 100]: ${s}. Clamped to ${s2}`);
-    }
-    if (l < 0 || l > 100) {
-      console.warn(`l is outside of [0, 100]: ${l}. Clamped to ${l2}`);
-    }
-    if (a < 0 || a > 1) {
-      console.warn(`a is outside of [0, 1]: ${a}. Clamped to ${a2}`);
-    }
     this.h = clamp(0, 360, h);
     this.s = clamp(0, 100, s);
     this.l = clamp(0, 100, l);
     this.a = clamp(0, 1, a);
+    if (h < 0 || h > 360) {
+      console.warn(`h is outside of [0, 360]: ${h}. Clamped to ${this.h}`);
+    }
+    if (s < 0 || s > 100) {
+      console.warn(`s is outside of [0, 100]: ${s}. Clamped to ${this.s}`);
+    }
+    if (l < 0 || l > 100) {
+      console.warn(`l is outside of [0, 100]: ${l}. Clamped to ${this.l}`);
+    }
+    if (a < 0 || a > 1) {
+      console.warn(`a is outside of [0, 1]: ${a}. Clamped to ${this.a}`);
+    }
     return ;
   }
 

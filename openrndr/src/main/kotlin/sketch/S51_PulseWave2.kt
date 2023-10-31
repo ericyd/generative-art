c29f7d82acf5ab4f69cba0899958c7ab95c362ca
Eric Dauenhauer <eric@ericyd.com>
2023-10-31 12:35:20 -0500

    Pulse wave v2!!!

--- openrndr/src/main/kotlin/sketch/S51_PulseWave2.kt
+++ openrndr/src/main/kotlin/sketch/S51_PulseWave2.kt
@@ -2,11 +2,13 @@ package sketch
 
 import org.openrndr.application
 import org.openrndr.color.ColorRGBa
+import org.openrndr.draw.Drawer
 import org.openrndr.draw.LineCap
 import org.openrndr.extensions.Screenshots
 import org.openrndr.extra.noise.random
 import org.openrndr.extra.color.palettes.ColorSequence
 import org.openrndr.extra.fx.edges.Contour
+import org.openrndr.extra.noise.gaussian
 import org.openrndr.extra.shapes.toRounded
 import org.openrndr.math.*
 import org.openrndr.math.transforms.rotateZ
@@ -32,7 +34,7 @@ fun main() = application {
     val screenshots = extend(Screenshots()) {
       quitAfterScreenshot = false
       contentScale = 3.0
-      captureEveryFrame = false
+      captureEveryFrame = true
       name = "screenshots/$progName/${timestamp()}-seed-$seed.jpg"
     }
 
@@ -51,58 +53,51 @@ fun main() = application {
       "005D7E",
     )
 
-    extend {
-      val rng = Random(seed)
-      val center = Vector2(width * 0.5, height * 0.5)
-      val spectrum = ColorSequence(colors.shuffled(rng).mapIndexed { index, hex ->
-        Pair(map(0.0, colors.size - 1.0, 0.0, 1.0, index.toDouble()), ColorRGBa.fromHex(hex))
-      })
-      drawer.stroke = ColorRGBa.BLACK
-      drawer.strokeWeight = 5.0
+    /**
+     * A wave pulse is given by the equation y=f(x,t)=Aexp(−B(x−vt)^2).
+     * https://www.toppr.com/ask/en-us/question/a-wave-pulse-is-given-by-the-equation-yfxta-expbxvt2-given-a10mb10m2-and-v20ms-which/
+     *
+     * @param x the position on the x-axis at which to calculate the pulse function
+     * @param t the "x offset". The pulse formula naturally centers at the origin,
+     *          so `t` allows the pulse to occur in other spacial locations.
+     * @param A amplitude. When negative, the pulse goes "up" since
+     *          OPENRNDR has origin at upper left.
+     * @return the `y` coordinate for the pulse function
+     */
+    fun pulse(x: Double, t: Double, A: Double): Double {
+      // "spread" - lower is more spread out, higher is tighter
+      val B = 0.00000001
+      // the "squareness" -- higher is more square
+      // 3.5 is kinda interesting
+      val exp = 3.0
+      return A * E.pow(-B * (x - t).pow(exp))
+    }
 
-      /**
-       * A wave pulse is given by the equation y=f(x,t)=Aexp(−B(x−vt)^2).
-       * https://www.toppr.com/ask/en-us/question/a-wave-pulse-is-given-by-the-equation-yfxta-expbxvt2-given-a10mb10m2-and-v20ms-which/
-       *
-       * @param x the position on the x-axis at which to calculate the pulse function
-       * @param t the "x offset". The pulse formula naturally centers at the origin,
-       *          so `t` allows the pulse to occur in other spacial locations.
-       * @param A amplitude. When negative, the pulse goes "up" since
-       *          OPENRNDR has origin at upper left.
-       * @return the `y` coordinate for the pulse function
-       */
-      fun pulse(x: Double, t: Double, A: Double): Double {
-        // "spread" - lower is more spread out, higher is tighter
-        val B = 0.00000009
-        // the "squareness" -- higher is more square
-        // 3.5 is kinda interesting
-        val exp = 4.0
-        return A * E.pow(-B * (x - t).pow(exp))
-      }
+    fun waveSet(tMin: Double,
+                tMax: Double,
+                A: Double,
+                /**
+                 * Pair is of <Point, Derivative at that point in radians>
+                 */
+                baseline: Sequence<Pair<Vector2, Double>>,
+                stepSize: Double,
+                rotateAbout: Vector2,
+                spectrum: ColorSequence,
+                rng: Random): Sequence<(d: Drawer) -> Unit> {
+      // currentContour gets merged with "newContour" to create a closed ShapeContour that can be filled
+      // each contour itself is just a single line of the wave, so we must join two offset contours
+      // to create a single fillable shape
+      var currentContour: ShapeContour = contour {  }
 
-      fun waveSet(tMin: Double,
-                  tMax: Double,
-                  A: Double,
-                  /**
-                   * Pair is of <Point, Derivative at that point in radians>
-                   */
-                  baseline: Sequence<Pair<Vector2, Double>>,
-                  stepSize: Double = 30.0) {
-        // currentContour gets merged with "newContour" to create a closed ShapeContour that can be filled
-        // each contour itself is just a single line of the wave, so we must join two offset contours
-        // to create a single fillable shape
-        var currentContour: ShapeContour = contour {  }
+      // each waveSet pulls from a small portion of the total spectrum.
+      val spectrumMin = random(0.0, 0.75, rng)
+      val spectrumMax = spectrumMin + 0.25
 
-        // each waveSet pulls from a small portion of the total spectrum.
-        val spectrumMin = random(0.0, 0.75, rng)
-        val spectrumMax = spectrumMin + 0.25
-
+      return sequence {
         // move successively through the values of "t",
         // where "t" is the peak of the wave pulse
         var t = tMin
         while (t < tMax) {
-          // center the line in the canvas
-          val yOffset = height * 0.5
           var newContour = contour {
             val (start) = baseline.first()
             moveTo(start.x, start.y)
@@ -112,13 +107,10 @@ fun main() = application {
               // val y = pulse(point.x, t, A) + point.y
               // lineTo(Vector2(point.x, y))
 
-
-//              val y = pulse(point.x, t, A) + yOffset
               val y = pulse(point.x, t, A) + point.y
-              // rotating about `point` vs `center` is a **substantial** difference.
-              // rotating about `center` is probably "cooler" but very unexpected patterns
-              val contourPoint = rotatePoint(Vector2(point.x, y), derivative, center)
-//              val contourPoint = rotatePoint(Vector2(point.x, y), derivative, point)
+              // rotating about `point` vs a fixed point is a **substantial** difference.
+              // rotating about a fixed point is probably "cooler" but very unexpected patterns
+              val contourPoint = rotatePoint(Vector2(point.x, y), derivative, rotateAbout)
               lineTo(contourPoint)
 
               // debugging: draw the "baseline"
@@ -129,27 +121,32 @@ fun main() = application {
           // once we've established at least one base contour, we can start combining them
           // and drawing our fillable shape contours
           if (!currentContour.empty) {
-            drawer.fill = spectrum.index(map(tMin, tMax, spectrumMin, spectrumMax, t))
+            yield { drawer: Drawer ->
+              drawer.fill = spectrum.index(map(tMin, tMax, spectrumMin, spectrumMax, t))
 
-            drawer.contour(
-              ShapeContour.fromPoints(
-                // I **think** "asReversed" might be more efficient than "reversed" but not 100% sure
-                currentContour.segments.map { it.start } + newContour.segments.asReversed().map { it.start },
-                closed = true
+              drawer.contour(
+                ShapeContour.fromPoints(
+                  // I **think** "asReversed" might be more efficient than "reversed" but not 100% sure
+                  currentContour.segments.map { it.start } + newContour.segments.asReversed().map { it.start },
+                  closed = true
+                )
               )
-            )
+            }
           }
           currentContour = newContour
           t += stepSize
         }
       }
+    }
 
-      val baseline = sequence {
-        // for now, let's keep it a simple sine wave
+    fun baseline(waveBaseline: Double = 0.0, phase: Double, majorScale: Double, minorAmplitude: Double, minorScale: Double): Sequence<Pair<Vector2, Double>> {
+      return sequence {
         val waveAmplitude = height / 3.0
-        val waveBaseline = height / 2.0
-        val xScale = 1.0 / width * 3.5
-        val wave = { x: Double -> sin(x * xScale) * waveAmplitude + waveBaseline }
+        val wave = { x: Double ->
+          val majorWave = sin(x * majorScale + phase)
+          val minorWave = sin(x * minorScale + phase) * minorAmplitude
+          (majorWave + minorWave) * waveAmplitude + waveBaseline
+        }
 
         // previous point is used to calculate derivative;
         // starts at the "first" position
@@ -164,22 +161,48 @@ fun main() = application {
           yield(Pair(previousPoint, derivative))
         }
       }
+    }
 
-      waveSet(
-        tMin = width * -0.25,
-        tMax = width * 1.625,
-        A = height * 0.25,
-        baseline,
-        stepSize = 30.0
-      )
+    extend {
+      // wow not sure if other seeds will be interesting but this one ROCKS
+//      seed = 1808905369
+//      seed = 1703181398
+//      seed = 1252262956
+      val rng = Random(seed)
+      val center = Vector2(width * 0.5, height * 0.5)
+      val spectrum = ColorSequence(colors.shuffled(rng).mapIndexed { index, hex ->
+        Pair(map(0.0, colors.size - 1.0, 0.0, 1.0, index.toDouble()), ColorRGBa.fromHex(hex))
+      })
+      drawer.stroke = ColorRGBa.BLACK
+      drawer.strokeWeight = 2.0
 
-      waveSet(
-        tMin = width * -0.325,
-        tMax = width * 1.75,
-        A = height * -0.25,
-        baseline,
-        stepSize = 30.0
-      )
+      val stepSize = 30.0
+      val rotationAxis = Vector2.gaussian(center, center * 0.41, rng) // center
+
+      /* wave props */
+      val phase = random(-PI, PI, rng)
+      val majorScale = 1.0 / width * 2.25
+      val minorAmplitude = random(0.1, 0.2, rng)
+      val minorScale = majorScale * random(2.2, 3.3, rng)
+      var offset = random(height * 0.1, height * 0.5, rng)
+      /* end wave props */
+      val waveAmplitudes = List(4) { random(height * 0.05, height * 0.2,  rng) }
+      for (waveAmplitude in waveAmplitudes) {
+        val waveSequence = waveSet(
+          tMin = width * -0.25,
+          tMax = width * 1.625,
+          A = waveAmplitude,
+          baseline(offset, phase, majorScale, minorAmplitude, minorScale),
+          stepSize,
+          rotationAxis,
+          spectrum,
+          rng
+        )
+        for (waveFn in waveSequence) {
+          waveFn(drawer)
+        }
+        offset += waveAmplitude
+      }
 
       if (screenshots.captureEveryFrame) {
         seed = random(0.0, Int.MAX_VALUE.toDouble()).toInt()

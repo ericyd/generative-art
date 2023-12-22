44e022ac7d76847b6319cb432d854da183a4b1a3
Eric Dauenhauer <eric@ericyd.com>
2023-12-22 17:02:17 -0600

    improvements

--- homegrown-svg/sketch/231221-random-squares.js
+++ homegrown-svg/sketch/231221-random-squares.js
@@ -10,26 +10,36 @@ const config = {
 }
 
 let seed =  randomSeed()
-// seed = 7863031615148757
 
 renderSvg(config, (svg) => {
   svg.filenameMetadata = { seed }
   const rng = createRng(seed)
 
-  const columnCount = 32
-  const rowCount = 32
-  const maxDimension = 6
+  svg.fill = null
+  svg.stroke = ColorHex.Black
+  svg.strokeWidth = 0.15
+
+  populateGrid(svg, rng, {
+    xStart: 0,
+    yStart: 0,
+    columnCount: 32,
+    rowCount: 32,
+  })  
+
+  return () => {
+    seed = randomSeed()
+  }
+})
+
+function populateGrid(svg, rng, { xStart, yStart, columnCount, rowCount }) {
+  const maxDimension = Math.min(6, columnCount, rowCount)
   const xCell = svg.width / columnCount
   const yCell = svg.height / rowCount
   const cornerRadius = hypot(xCell, yCell) * 0.15
   const padding = cornerRadius / 2.0
   const grid = new Grid({ columnCount, rowCount })
-  svg.fill = null
-  svg.stroke = ColorHex.Black
-  svg.strokeWidth = 0.15
-
+  
   for (const [{ x, y }, isOccupied] of grid) {
-    console.log(x,y)
     if (isOccupied) {
       continue
     }
@@ -41,14 +51,14 @@ renderSvg(config, (svg) => {
     if (dim.x == dim.y && random(0.0, 1.0, rng) < 0.1) {
       const radius = (dim.x / 2.0 * xCell) - padding
       svg.circle(circle(
-        x * xCell + radius + padding,
-        y * yCell + radius + padding,
+        xStart + x * xCell + radius + padding,
+        yStart + y * yCell + radius + padding,
         radius
       ))
     } else {
       const r = rect(
-        x * xCell + padding,
-        y * yCell + padding,
+        xStart + x * xCell + padding,
+        yStart + y * yCell + padding,
         dim.x * xCell - padding * 2.0,
         dim.y * yCell - padding * 2.0,
       )
@@ -63,14 +73,20 @@ renderSvg(config, (svg) => {
       }
     }
   }
+}
 
-  return () => {
-    seed = randomSeed()
-  }
-})
-
-// This will find all available sizes without any chance of overlaps
 /**
+ * This will find all available sizes without any chance of overlaps
+ * 
+ * Algo:
+ * 1. Starting at [0, 0], move diagonally to "maxDimension".
+ * 2. At each diagonal point, extend in the x and y axes to the maxDimension.
+ *    2.a. A each cell, check if it is occupied
+ *      - If a cell is occupied, update the "max" for the given axis
+ *      - Else, add the coordinate pair to the list of available sizes
+ * 3. Return the list of available sizes
+ * 
+ * One problem with this algorithm is that point [diagonal, diagonal] gets added twice every iteration.
  * 
  * @param {Grid} grid 
  * @param {Integer} x 
@@ -82,46 +98,33 @@ function findAvailableSizes(grid, x, y, maxDimension) {
   // compute the available sizes, e.g. 1x1, 1x2, 2x2, 2x3, etc, up to 4x4
   const availableSizes = []
 
-
-  // ok let's try a different algorithm
-  // rather than going left to right, top to bottom, let's go diagonally, then grow outward from each diagonal.
-  // at each diagonal, we can identify if we're above the maxX or maxY and stop at the appropriate place
-  let maxX = maxDimension
-  let maxY = maxDimension
+  // avoid extending beyond the grid
+  let maxX = Math.min(grid.columnCount, x + maxDimension) - x
+  let maxY = Math.min(grid.rowCount, y + maxDimension) - y
   for (const diagonal of range(0, maxDimension)) {
-    // if diagonal is ever greater than our max X or Y dimension, then we've already found all our valid pairs
-    if (diagonal > maxX || diagonal > maxY) {
+    // if diagonal is ever greater than or equal to our max X or Y dimension,
+    // then we've already found all our valid pairs
+    if (diagonal >= maxX || diagonal >= maxY) {
       break
     }
+    // extend from "diagonal" to "max" in the x axis
     for (const i of range(diagonal, maxX)) {
       const j = diagonal
-      // outside grid dimensions, continue
-      if (i + x > grid.columnCount - 1 || j + y > grid.rowCount - 1) {
-        continue
-      }
       if (!grid.get(i + x, j + y)) {
         availableSizes.push(vec2(i + 1, j + 1))
       } else {
-        // TODO: including the "-1" fixes any overlaps, but it creates a strange artifact where things are predominantly oriented in the grid's `order` direction
-        // One hypothesis is that, when the first row is filled, it creates irregularities that are harder to fill later.
-        // This almost makes me think that I need to find a quasi-random way to select starting points to fill out this grid.
-        // It's almost like there is a selection bias against certain orientations.
-        maxX = Math.min(maxX, i - 1)
+        maxX = Math.min(maxX, i)
         break
       }
     }
 
     // extend from "diagonal" to "max" in the y axis
-    for (const j of range(diagonal + 1, maxY)) {
+    for (const j of range(diagonal, maxY)) {
       const i = diagonal
-      // outside grid dimensions, continue
-      if (i + x > grid.columnCount - 1 || j + y > grid.rowCount - 1) {
-        continue
-      }
       if (!grid.get(i + x, j + y)) {
         availableSizes.push(vec2(i + 1, j + 1))
       } else {
-        maxY = Math.min(maxY, j - 1)
+        maxY = Math.min(maxY, j)
         break
       }
     }

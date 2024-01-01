8ecab67b460454a20d850eece89c7bc3e2186831
Eric Dauenhauer <eric@ericyd.com>
2024-01-01 14:28:01 -0600

    Genuary day 02 fixup

--- homegrown-svg/sketch/231227-Genuary2024_02.js
+++ homegrown-svg/sketch/231227-Genuary2024_02.js
@@ -10,18 +10,19 @@
  * Generative colors, procedural colors, emergent colors.
  * """
  */
-import { vec2, randomSeed, createRng, Vector2, random, circle, ColorRgb, randomFromArray, rect, hypot, Grid, range, Oscillator, ColorHsl, hsl, path, FractalizedLine, cos, sin } from 'artsvg'
+import { PHI, vec2, randomSeed, createRng, Vector2, random, circle, ColorRgb, randomFromArray, rect, hypot, Grid, range, Oscillator, ColorHsl, hsl, path, FractalizedLine, cos, sin } from 'artsvg'
 import { renderSvg } from 'artsvg/render'
 
 const config = {
   background: '#fff',
   width: 100,
   height: 100,
-  scale: 5,
+  scale: 10,
   loopCount: 1,
 }
 
-let seed =  randomSeed()
+let seed = randomSeed()
+seed = 1557764099652407
 
 renderSvg(config, (svg) => {
   const rng = createRng(seed)
@@ -31,13 +32,15 @@ renderSvg(config, (svg) => {
   svg.strokeWidth = 0.15
   const columnCount = 3
   const rowCount = 3
+  let baseHue = random(0, 310, rng)
 
   for (const [gridPoint] of new Grid({ columnCount, rowCount })) {
     // center each point in a grid of the canvas dimensions
     const x = gridPoint.x * svg.width / columnCount + svg.width / (columnCount * 2)
     const y = gridPoint.y * svg.height / rowCount + svg.height / (rowCount * 2)
 
-    const baseHue = random(0, 310, rng)
+    baseHue += 360 * (PHI - 1)
+    while (baseHue > 360) { baseHue -= 360 }
     const baseSaturation = random(0.25, 0.75, rng)
     const baseLightness = random(0.35, 0.95, rng)
 
@@ -50,7 +53,9 @@ renderSvg(config, (svg) => {
         }
       })
       const line = new FractalizedLine(p.points, rng)
-      svg.stroke = hsl(random(baseHue, baseHue + 50, rng), random(baseSaturation, baseSaturation + 0.25, rng), random(baseLightness, baseLightness - 0.25, rng))
+      let hue = random(baseHue, baseHue + 50, rng)
+      while (hue > 360) { hue -= 360 }
+      svg.stroke = hsl(hue, random(baseSaturation, baseSaturation + 0.25, rng), random(baseLightness, baseLightness - 0.25, rng))
       svg.path(line.perpendicularSubdivide(10, 0.25).path())
     }
   }

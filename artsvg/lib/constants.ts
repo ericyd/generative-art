29b963d54f4bfd7de5457231d85530887af9d8f5
Eric Dauenhauer <eric@ericyd.com>
2024-01-01 11:29:48 -0600

    add constants to artsvg lib

---
+++ artsvg/lib/constants.ts
@@ -0,0 +1,6 @@
+// some "useful" constants
+export const PI = Math.PI
+export const E = Math.E
+// from `0.5 + Math.pow(5, 0.5) * 0.5` or `Math.pow(Math.E, Math.asinh(0.5))`, credit https://www.goldennumber.net/math/
+export const PHI = 1.618033988749895
+export const TAU = 2 * PI

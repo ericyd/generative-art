ce9994f7b1bc7ca4b8ef1a6b5d7e37442eac612b
Eric Dauenhauer <eric@ericyd.com>
2024-01-08 12:16:18 -0600

    Genuary Day 8

--- homegrown-svg/sketch/240104-Genuary2024_08.js
+++ homegrown-svg/sketch/240104-Genuary2024_08.js
@@ -8,16 +8,26 @@
  * Chaotic system.
  * """
  */
-import { renderSvg, map, vec2, randomSeed, createRng, Vector2, random, circle, ColorRgb, randomFromArray, rect, hypot, Grid, range, hsl, lineSegment, Rectangle, randomInt, PI, cos, sin } from '@salamivg/core'
+import { renderSvg, map, vec2, randomSeed, createRng, Vector2, random, circle, ColorRgb, randomFromArray, rect, hypot, Grid, range, hsl, lineSegment, Rectangle, randomInt, PI, cos, sin, clamp, ColorSequence, shuffle } from '@salamivg/core'
 
 const config = {
   width: 100,
   height: 100,
-  scale: 6,
-  loopCount: 5,
+  scale: 10,
+  loopCount: 1,
 }
 
 let seed = randomSeed()
+seed = 1546416152405953
+
+const colors = [
+  '#B9D6F5',
+  '#D09AEA',
+  '#CB769E',
+  '#E09891',
+  '#E8B9AB',
+  '#C2E2B2'
+]
 
 /**
  * Rules
@@ -34,105 +44,45 @@ let seed = randomSeed()
  *       and the angle range is inverted, i.e. starts at [PI/2, PI/2] and ends at [3*PI/2, -PI/2]
  */
 
-// this one goes top to bottom, I wanted bottom to top
-renderSvg.skip(config, (svg) => {
-  svg.filenameMetadata = { seed }
-  const rng = createRng(seed)
-
-  svg.fill = null
-  svg.stroke = ColorRgb.Black
-  svg.strokeWidth = 0.15
-
-  const nLines = randomInt(6, 40, rng)
-  for (const i of range(0, nLines)) {
-    const x = map(0, nLines, 0, svg.width, i)
-    const stepsize = svg.height / 50
-    svg.path(path => {
-      path.moveTo(vec2(x, 0))
-      for (const y of range(stepsize, svg.height, stepsize)) {
-        const start = path.cursor
-        const end = vec2(x, y) // alternative, top to bottom
-        const length = end.subtract(start).length()
-
-        const control1Angle = random(
-          map(0, svg.height, -PI/2, -3*PI/2, y),
-          map(0, svg.height, -PI/2, PI/2, y),
-          rng
-        )
-        const control1Length = random(
-          map(0, svg.height, length * 0.1, length * 5, y),
-          map(0, svg.height, length * 0.2, length * 10, y),
-          rng
-        )
-        const control1 = start.add(vec2(cos(control1Angle), sin(control1Angle)).multiply(control1Length))
-
-        const control2Angle = random(
-          map(0, svg.height, PI/2, 3*PI/2, y),
-          map(0, svg.height, PI/2, -PI/2, y),
-          rng
-        )
-        const control2Length = random(
-          map(0, svg.height, length * 0.1, length * 5, y),
-          map(0, svg.height, length * 0.2, length * 10, y),
-          rng
-        )
-        const control2 = end.add(vec2(cos(control2Angle), sin(control2Angle)).multiply(control2Length))
-        path.cubicBezier(control1, control2, end)
-      }
-    })
-  }
-
-  return () => {
-    seed = randomSeed()
-  }
-})
-
-
 renderSvg(config, (svg) => {
   svg.filenameMetadata = { seed }
   const rng = createRng(seed)
+  svg.setBackground('#28466C')
 
   svg.fill = null
   svg.stroke = ColorRgb.Black
-  svg.strokeWidth = 0.15
+  svg.strokeWidth = 0.5
 
-  const nLines = randomInt(6, 40, rng)
+  const spectrum = ColorSequence.fromHexes(shuffle(colors, rng))
+
+  const nLines = randomInt(15, 25, rng)
   for (const i of range(0, nLines)) {
-    const x = map(0, nLines, 0, svg.width, i)
+    const x = map(0, nLines, svg.width / nLines, svg.width, i)
     const stepsize = svg.height / 50
     svg.path(path => {
+      path.stroke = spectrum.at(random(0, 1, rng))
       path.moveTo(vec2(x, svg.height))
       for (const y of range(stepsize, svg.height, stepsize)) {
         const start = path.cursor
         const end = vec2(x, svg.height - y)
         const length = end.subtract(start).length()
 
-        const control1Angle = random(
-          map(svg.height, 0, -PI/2, -3*PI/2, svg.height - y),
-          map(svg.height, 0, -PI/2, PI/2, svg.height - y),
+        const control1Angle = random(-3*PI/2, PI/2, rng)
+        const control2Angle = random(3*PI/2, -PI/2, rng)
+        const control1Length = clamp(0, 100, random(
+          map(svg.height * 0.7, 0, length * 0.1, length * 5, svg.height - y),
+          map(svg.height * 0.7, 0, length * 0.2, length * 10, svg.height - y),
           rng
-        )
-        const control2Angle = random(
-          map(svg.height, 0, PI/2, 3*PI/2, svg.height - y),
-          map(svg.height, 0, PI/2, -PI/2, svg.height - y),
+        ))
+        const control2Length = clamp(0, 100, random(
+          map(svg.height * 0.85, 0, length * 0.1, length * 5, svg.height - y),
+          map(svg.height * 0.85, 0, length * 0.2, length * 10, svg.height - y),
           rng
-        )
-        const control1Length = random(
-          map(svg.height, 0, length * 0.1, length * 5, svg.height - y),
-          map(svg.height, 0, length * 0.2, length * 10, svg.height - y),
-          rng
-        )
+        ))
 
         // swapping control angles makes this more "loopy", though it doesn't fit my original rule set
         // I think there's room for improvement here
         const control1 = start.add(vec2(cos(control2Angle), sin(control2Angle)).multiply(control1Length))
-
-        
-        const control2Length = random(
-          map(svg.height, 0, length * 0.1, length * 5, svg.height - y),
-          map(svg.height, 0, length * 0.2, length * 10, svg.height - y),
-          rng
-        )
         const control2 = end.add(vec2(cos(control1Angle), sin(control1Angle)).multiply(control2Length))
         path.cubicBezier(control1, control2, end)
       }
